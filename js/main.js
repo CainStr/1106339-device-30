319087c9c9326014f095b044afe351fa1b0f3c06
CainStr <77696629+CainStr@users.noreply.github.com>
2021-03-24 00:01:36 +0300

    Исправил б12, б14, б15

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 // modalWriteUs
 document.addEventListener("DOMContentLoaded", ()=>{
   console.log("ok");
-  const writeUs = document.getElementById("writeUs")
-  const modalWriteUs=document.getElementById("modalWriteUs")
-  writeUs.addEventListener("click", (event)=>{
-    modalWriteUs.classList.add("visible")
+  const write__us = document.getElementById("write__us")
+  const modal__write__us=document.getElementById("modal__write__us")
+  write__us.addEventListener("click", (event)=>{
+    modal__write__us.classList.add("visible")
     event.preventDefault();
-    const modalClose=document.getElementById("modalClose")
-    modalClose.addEventListener("click",()=>{
-      modalWriteUs.classList.remove("visible");
+    const modalClose=document.getElementById("modal__close")
+    modal__close.addEventListener("click",()=>{
+      modal__write__us.classList.remove("visible");
     })
   })
 });
 
 document.addEventListener("DOMContentLoaded", ()=>{
   console.log("ok");
-  const mapBig = document.getElementById("mapBig")
-  const modalMap=document.getElementById("modalMap")
-  mapBig.addEventListener("click", (event)=>{
-    modalMap.classList.add("visible")
+  const map__big = document.getElementById("map__big")
+  const modal__map=document.getElementById("modal__map")
+  map__big.addEventListener("click", (event)=>{
+    modal__map.classList.add("visible")
     event.preventDefault();
-    const modalMapClose=document.getElementById("modalMapClose")
-    modalMapClose.addEventListener("click",()=>{
-      modalMap.classList.remove("visible");
+    const modal__map__close=document.getElementById("modal__map__close")
+    modal__map__close.addEventListener("click",()=>{
+      modal__map.classList.remove("visible");
     })
   })
 });
